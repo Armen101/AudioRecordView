8a24352f888f35f3f891fc6579f329b962cc7c29
Armen <gevorgyanweb@gmail.com>
2020-01-11 15:17:54 +0400

    implemented "Soft transition"

--- audio/src/main/java/com/visualizer/amplitude/AudioRecordView.kt
+++ audio/src/main/java/com/visualizer/amplitude/AudioRecordView.kt
@@ -11,7 +11,7 @@ import java.util.*
 
 class AudioRecordView : View {
 
-    enum class AlignTo(var value: Int){
+    enum class AlignTo(var value: Int) {
         CENTER(1),
         BOTTOM(2)
     }
@@ -21,12 +21,14 @@ class AudioRecordView : View {
     var chunkAlignTo = AlignTo.CENTER
 
     private val chunkPaint = Paint()
+    private var lastUpdateTime = 0L
 
     private var usageWidth = 0f
     private var chunkHeights = ArrayList<Float>()
     private var chunkWidths = ArrayList<Float>()
     private var topBottomPadding = 6.dp()
 
+    var chunkSoftTransition = false
     var chunkColor = Color.RED
         set(value) {
             chunkPaint.color = value
@@ -76,6 +78,7 @@ class AudioRecordView : View {
     fun update(fft: Int) {
         handleNewFFT(fft)
         invalidate() // call to the onDraw function
+        lastUpdateTime = System.currentTimeMillis()
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -95,15 +98,22 @@ class AudioRecordView : View {
         ).apply {
             try {
                 chunkSpace = getDimension(R.styleable.AudioRecordView_chunkSpace, chunkSpace)
-                chunkMaxHeight = getDimension(R.styleable.AudioRecordView_chunkMaxHeight, chunkMaxHeight)
-                chunkMinHeight = getDimension(R.styleable.AudioRecordView_chunkMinHeight, chunkMinHeight)
-                chunkRoundedCorners = getBoolean(R.styleable.AudioRecordView_chunkRoundedCorners, chunkRoundedCorners)
+                chunkMaxHeight =
+                    getDimension(R.styleable.AudioRecordView_chunkMaxHeight, chunkMaxHeight)
+                chunkMinHeight =
+                    getDimension(R.styleable.AudioRecordView_chunkMinHeight, chunkMinHeight)
+                chunkRoundedCorners =
+                    getBoolean(R.styleable.AudioRecordView_chunkRoundedCorners, chunkRoundedCorners)
                 chunkWidth = getDimension(R.styleable.AudioRecordView_chunkWidth, chunkWidth)
                 chunkColor = getColor(R.styleable.AudioRecordView_chunkColor, chunkColor)
-                chunkAlignTo = when (getInt(R.styleable.AudioRecordView_chunkAlignTo, chunkAlignTo.ordinal)) {
+                chunkAlignTo =
+                    when (getInt(R.styleable.AudioRecordView_chunkAlignTo, chunkAlignTo.ordinal)) {
                         AlignTo.BOTTOM.value -> AlignTo.BOTTOM
                         else -> AlignTo.CENTER
                     }
+
+                chunkSoftTransition =
+                    getBoolean(R.styleable.AudioRecordView_chunkSoftTransition, chunkSoftTransition)
 
                 setWillNotDraw(false)
                 chunkPaint.isAntiAlias = true
@@ -145,9 +155,11 @@ class AudioRecordView : View {
 
         var fftPoint = fft / point
 
-        if (chunkHeights.isNotEmpty()) {
+        if (chunkSoftTransition && chunkHeights.isNotEmpty()) {
+            val updateTimeInterval = System.currentTimeMillis() - lastUpdateTime
+            val scaleFactor = calculateScaleFactor(updateTimeInterval)
             val prevFftWithoutAdditionalSize = chunkHeights.last() - chunkMinHeight
-            fftPoint = fftPoint.softTransition(prevFftWithoutAdditionalSize, 2.5f, 3f)
+            fftPoint = fftPoint.softTransition(prevFftWithoutAdditionalSize, 2.2f, scaleFactor)
         }
 
         fftPoint += chunkMinHeight
@@ -159,6 +171,18 @@ class AudioRecordView : View {
         }
 
         chunkHeights.add(chunkHeights.size, fftPoint)
+    }
+
+    private fun calculateScaleFactor(updateTimeInterval: Long): Float {
+        return when (updateTimeInterval) {
+            in 0..50 -> 1.6f
+            in 50..100 -> 2.2f
+            in 100..150 -> 2.8f
+            in 100..150 -> 3.4f
+            in 150..200 -> 4.2f
+            in 200..500 -> 4.8f
+            else -> 5.4f
+        }
     }
 
     private fun drawChunks(canvas: Canvas) {
